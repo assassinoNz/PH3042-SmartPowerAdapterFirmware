c64cd05e1b01b61d9a22f951f1a6402875c84570
assassino <nirmaldiaz@gmail.com>
2023-02-07 11:52:08 +0530

    Added: OTA update feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,85 @@
 #include <Arduino.h>
+#include <LittleFS.h>
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncMqttClient.h>
 #include <ArduinoJson.h>
-#include <LittleFS.h>
+#include <ESP8266httpUpdate.h>
 
-const char *MQTT_HOST = "smart-power-adapter-server.fly.dev";
-const short MQTT_HOST_PORT = 1883;
-const char *MQTT_HOST_USERNAME = "assassino";
-const char *MQTT_HOST_PASSWORD = "assassino@HiveMQ";
-const char *MQTT_CLIENT_ID = "QEIZrUmZGUuzBqRnw0jZ";
-
-const char INDEX_HTML[] PROGMEM = R"rawliteral(
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <h1>Hello World</h1>
-    </body>
-    </html>
-)rawliteral";
-
-AsyncMqttClient mqttClient;
-AsyncWebServer httpServer(80);
-
-const unsigned char PIN_RESET = D4;
-
-void onMqttConnect(bool sessionPresent) {
-    Serial.println("\n[MQTT]: Established connection HOST: " + String(MQTT_HOST) + ":" + String(MQTT_HOST_PORT));
-
-    mqttClient.subscribe("power", 0);
+namespace IO {
+    const unsigned char PIN_RESET = D4;
 }
 
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-    Serial.println("\n[MQTT]: Disconnected BECAUSE: ");
+namespace LFS {
+    const char *SELF_AP_CREDENTIALS_PATH = "/HomeAPCredentials.txt";
 }
 
-void onMqttPublish(uint16_t packetId) {
-    Serial.println("\n[MQTT]: Published PACKET_ID: " + String(packetId));
+namespace WIFI {
+    WiFiClient client;
 }
 
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-    Serial.println("\n[MQTT]: Subscribed PACKET_ID: " + String(packetId) + " QOS: " + qos);
+namespace HTTP {
+    AsyncWebServer server(80);
+
+    const char *HOST = "192.168.1.4";
+    const short HOST_PORT = 8080;
+    const char *FIRMWARE_URL = "/update/firmware.bin";
+    const char SELF_AP_HTML[] PROGMEM = R"rawliteral(
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+        </body>
+        </html>
+    )rawliteral";
 }
 
-void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-    Serial.println("\n[MQTT]: Recieved TOPIC: " + String(topic) + " PAYLOAD: " + String(payload));
+namespace MQTT {
+    AsyncMqttClient client;
 
-    if (strcmp(topic, "power") == 0) {
-        DynamicJsonDocument message(1024);
-        deserializeJson(message, payload);
+    const char *HOST = "192.168.1.4";
+    const short HOST_PORT = 1883;
+    const char *HOST_USERNAME = "assassino";
+    const char *HOST_PASSWORD = "assassino@HiveMQ";
+    const char *CLIENT_ID = "QEIZrUmZGUuzBqRnw0jZ";
 
-        const bool state = message["state"];
-        pinMode(LED_BUILTIN, OUTPUT);
-        digitalWrite(LED_BUILTIN, !state); //WARNING: LED_BUILTIN seems to be active low
+    void onMqttConnect(bool sessionPresent) {
+        Serial.println("\n[MQTT]: Established connection HOST: " + String(MQTT::HOST) + ":" + String(MQTT::HOST_PORT));
+
+        client.subscribe("power", 0);
+    }
+
+    void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+        Serial.print("\n[MQTT]: Disconnected BECAUSE: " + String((unsigned char) reason));
+    }
+
+    void onMqttPublish(uint16_t packetId) {
+        Serial.println("\n[MQTT]: Published PACKET_ID: " + String(packetId));
+    }
+
+    void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+        Serial.println("\n[MQTT]: Subscribed PACKET_ID: " + String(packetId) + " QOS: " + qos);
+    }
+
+    void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+        Serial.println("\n[MQTT]: Recieved TOPIC: " + String(topic) + " PAYLOAD: " + String(payload));
+
+        if (strcmp(topic, "power") == 0) {
+            DynamicJsonDocument message(1024);
+            deserializeJson(message, payload);
+
+            const bool state = message["state"];
+            pinMode(LED_BUILTIN, OUTPUT);
+            digitalWrite(LED_BUILTIN, !state); //WARNING: LED_BUILTIN seems to be active low
+        }
     }
 }
 
@@ -72,64 +92,81 @@ void setup() {
     //LittleFS
     LittleFS.begin();
 
-    // if (digitalRead(PIN_RESET) == HIGH) {
+    // if (digitalRead(IO::PIN_RESET) == HIGH) {
     //     //CASE: Reset pin is active
-    //     LittleFS.remove("/HomeAPCredentials.txt");
+    //     LittleFS.remove(LFS::SELF_AP_CREDENTIALS_PATH);
     //     Serial.println("[SETUP]: Removed stored home AP credentials");
     // }
 
-    if (LittleFS.exists("/HomeAPCredentials.txt")) {
+    Serial.println("\n[SETUP]: Searching for stored credentials for your home AP");
+    if (LittleFS.exists(LFS::SELF_AP_CREDENTIALS_PATH)) {
         //CASE: Credentials for home AP is available
-        Serial.println("\n[SETUP]: Found stored credentials for a home AP");
-
-        File homeAPCredentials = LittleFS.open("/HomeAPCredentials.txt", "r");
+        File homeAPCredentials = LittleFS.open(LFS::SELF_AP_CREDENTIALS_PATH, "r");
         String homeApSSID = homeAPCredentials.readStringUntil(',');
         String homeApPSK = homeAPCredentials.readStringUntil(',');
         homeAPCredentials.close();
-        Serial.println(homeApSSID);
-        Serial.println(homeApPSK);
-        Serial.println("[SETUP]: Retrieved credentials SSID: " + homeApSSID + " PSK: " + homeApPSK);
+        Serial.println("[SETUP]: Stored credentials found SSID: " + homeApSSID + " PSK: " + homeApPSK);
 
         //WiFi
         Serial.println("[SETUP]: Connecting to WiFi SSID: " + homeApSSID);
-        WiFi.begin("Scorpius", "LogIn.WiFi.NSMTFS");
+        WiFi.begin(homeApSSID, homeApPSK);
         while (WiFi.status() != WL_CONNECTED) {
             Serial.print(".");
             delay(500);
         }
-        Serial.println("\n[SETUP]: Connected to WiFi SSID: " + homeApSSID);
+        Serial.println("\n[WIFI]: Connected to WiFi SSID: " + homeApSSID);
 
+        //Updates
+        Serial.println("[SETUP]: Checking for updates");
+        switch(ESPhttpUpdate.update(WIFI::client, HTTP::HOST, HTTP::HOST_PORT, HTTP::FIRMWARE_URL)) {
+            case HTTP_UPDATE_FAILED: {
+                Serial.println("[UPDATE] Failed");
+                break;
+            }
+            case HTTP_UPDATE_NO_UPDATES: {
+                Serial.println("[UPDATE] No updates available");
+                break;
+            }
+            case HTTP_UPDATE_OK: {
+                Serial.println("[UPDATE] Successful");
+                break;
+            }
+        }
+
+        //Time
+        Serial.println("[SETUP]: Configuring system time");
         if (time(NULL) < 1663485411) {
             //CASE: System time is not configured
             configTime(19800, 0, "pool.ntp.org");
-            Serial.println("[SETUP]: Configured system time HOST: pool.ntp.org");
+            Serial.println("[TIME]: Configured system time HOST: pool.ntp.org");
         }
 
-        mqttClient.setServer(MQTT_HOST, MQTT_HOST_PORT);
-        mqttClient.setClientId(MQTT_CLIENT_ID);
-        mqttClient.setCredentials(MQTT_HOST_USERNAME, MQTT_HOST_PASSWORD);
-        mqttClient.onConnect(onMqttConnect);
-        mqttClient.onDisconnect(onMqttDisconnect);
-        mqttClient.onPublish(onMqttPublish);
-        mqttClient.onSubscribe(onMqttSubscribe);
-        mqttClient.onMessage(onMqttMessage);
-        mqttClient.connect();
+        Serial.println("[SETUP]: Configuring MQTT");
+        MQTT::client.setServer(MQTT::HOST, MQTT::HOST_PORT);
+        MQTT::client.setClientId(MQTT::CLIENT_ID);
+        MQTT::client.setCredentials(MQTT::HOST_USERNAME, MQTT::HOST_PASSWORD);
+        MQTT::client.onConnect(MQTT::onMqttConnect);
+        MQTT::client.onDisconnect(MQTT::onMqttDisconnect);
+        MQTT::client.onPublish(MQTT::onMqttPublish);
+        MQTT::client.onSubscribe(MQTT::onMqttSubscribe);
+        MQTT::client.onMessage(MQTT::onMqttMessage);
+        MQTT::client.connect();
 
         while (true) {
-            mqttClient.publish("readings", 0, true, "[{\"v\":0.123,\"i\":0.345,\"time\":1674890175442},{\"v\":0.456,\"i\":0.456,\"time\":1674890175442},{\"v\":0.123,\"i\":0.345,\"time\":1674890175442},{\"v\":0.789,\"i\":0.567,\"time\":1674890175442}]");
+            MQTT::client.publish("readings", 0, true, "[{\"v\":0.123,\"i\":0.345,\"time\":1674890175442},{\"v\":0.456,\"i\":0.456,\"time\":1674890175442},{\"v\":0.123,\"i\":0.345,\"time\":1674890175442},{\"v\":0.789,\"i\":0.567,\"time\":1674890175442}]");
             delay(10000);
         }
     } else {
         //CASE: Create a softAP to change the authentication details
-        Serial.println("\n[SETUP]: No stored credentials for a home AP");
+        Serial.println("\n[SETUP]: No stored credentials found. COnfiguring self AP");
 
         WiFi.softAP("Smart Power Adapter", "123456789");
-        Serial.print("\n[SETUP]: Configured self AP SSID: Smart Power Adapter PSK: 123456789");
+        Serial.print("\n[SELF_AP]: Configured self AP SSID: Smart Power Adapter PSK: 123456789");
         
-        httpServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-            request->send_P(200, "text/html", INDEX_HTML);
+        HTTP::server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+            request->send_P(200, "text/html", HTTP::SELF_AP_HTML);
         });
-        httpServer.on("/connect", HTTP_GET, [](AsyncWebServerRequest *request) {
+        HTTP::server.on("/connect", HTTP_GET, [](AsyncWebServerRequest *request) {
             File homeAPCredentials = LittleFS.open("/HomeAPCredentials.txt", "w");
             String ssid = request->arg("ssid");
             String psk = request->arg("psk");
@@ -138,12 +175,12 @@ void setup() {
             homeAPCredentials.print(psk);
             homeAPCredentials.print(',');
             homeAPCredentials.close();
-            Serial.println("[SETUP]: Stored credentials for your home AP SSID: " + ssid + " PSK: " + psk);
-            request->send_P(200, "text/plain", "[SETUP]: Stored credentials for your home AP successfully");
+            Serial.println("[SELF_AP]: Stored credentials for your home AP SSID: " + ssid + " PSK: " + psk);
+            request->send_P(200, "text/plain", "[SELF_AP]: Stored credentials for your home AP successfully");
             ESP.restart();
         });
-        httpServer.begin();
-        Serial.println("\n[SETUP]: Configured HTTP httpServer PORT: 80");
+        HTTP::server.begin();
+        Serial.println("\n[SELF_AP]: Configured server PORT: 80");
     }
 }
 
