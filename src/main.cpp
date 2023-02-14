8a5828f4044349537449159f06a90a1fa8735e9d
assassino <nirmaldiaz@gmail.com>
2023-02-14 18:06:31 +0530

    Added: MQTTS support

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <LittleFS.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <AsyncMqttClient.h>
+#include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <ESP8266httpUpdate.h>
 
@@ -18,11 +18,11 @@ namespace LFS {
 }
 
 namespace WIFI {
-    WiFiClient client;
+    WiFiClientSecure client;
 }
 
 namespace HTTP {
-    const char *HOST = "192.168.1.4";
+    const char *HOST = "192.168.1.5";
     const short HOST_PORT = 8080;
     const char *FIRMWARE_URL = "/update/firmware.bin";
     const char SELF_AP_HTML[] PROGMEM = R"rawliteral(
@@ -33,37 +33,24 @@ namespace HTTP {
 }
 
 namespace MQTT {
-    const char *HOST = "192.168.1.4";
+    const char *HOST = "192.168.1.5";
     const short HOST_PORT = 1883;
     const char *HOST_USERNAME = "assassino";
-    const char *HOST_PASSWORD = "assassino@HiveMQ";
-    const char *CLIENT_ID = "<Read_From_LittleFS>";
+    const char *HOST_PASSWORD = "assassino";
+    const char *CLIENT_ID = "<CLIENT_ID>";
 
     const char *POWER_TOPIC = strcat((char *) CLIENT_ID, "/power");
     const char *READINGS_TOPIC = strcat((char *) CLIENT_ID, "/readings");
 
-    AsyncMqttClient client;
+    PubSubClient client(WIFI::client);
 
     void onMqttConnect(bool sessionPresent) {
         Serial.println("\n[MQTT]: Established connection HOST: " + String(MQTT::HOST) + ":" + String(MQTT::HOST_PORT));
-
         client.subscribe(POWER_TOPIC, 0);
     }
 
-    void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-        Serial.print("\n[MQTT]: Disconnected BECAUSE: " + String((unsigned char) reason));
-    }
-
-    void onMqttPublish(uint16_t packetId) {
-        Serial.println("\n[MQTT]: Published PACKET_ID: " + String(packetId));
-    }
-
-    void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-        Serial.println("\n[MQTT]: Subscribed PACKET_ID: " + String(packetId) + " QOS: " + qos);
-    }
-
-    void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
-        Serial.println("\n[MQTT]: Recieved TOPIC: " + String(topic) + " PAYLOAD: " + String(payload));
+    void onMqttMessage(const char* topic, byte* payload, unsigned int length) {
+        Serial.println("\n[MQTT]: Recieved TOPIC: " + String(topic) + " PAYLOAD: " + String((char*) payload));
 
         if (strcmp(topic, MQTT::POWER_TOPIC) == 0) {
             DynamicJsonDocument message(1024);
@@ -90,11 +77,15 @@ void setup() {
     //     LittleFS.remove(LFS::SELF_AP_CREDENTIALS_PATH);
     //     Serial.println("[LittleFS]: Removed all home AP credentials");
     // }
+    File mqttClientId = LittleFS.open(LFS::MQTT_CLIENT_ID_PATH, "w");
+    mqttClientId.println("abcdefghi");
+    mqttClientId.close();
 
     Serial.println("\n[SETUP]: Retrieving MQTT client ID");
     if (LittleFS.exists(LFS::MQTT_CLIENT_ID_PATH)) {
         File mqttClientId = LittleFS.open(LFS::MQTT_CLIENT_ID_PATH, "r");
-        MQTT::CLIENT_ID = mqttClientId.readString().c_str();
+        // MQTT::CLIENT_ID = (char *) mqttClientId.readString().c_str();
+        Serial.println("\n[LFS]: Retrieved MQTT client ID: " + String(MQTT::CLIENT_ID));
     } else {
         Serial.println("\n[LFS]: No MQTT client ID found. Please contact support");
         while (true);
@@ -104,13 +95,16 @@ void setup() {
     if (LittleFS.exists(LFS::SELF_AP_CREDENTIALS_PATH)) {
         //CASE: Credentials for home AP is available
         File homeAPCredentials = LittleFS.open(LFS::SELF_AP_CREDENTIALS_PATH, "r");
-        String homeApSSID = homeAPCredentials.readStringUntil(',');
-        String homeApPSK = homeAPCredentials.readStringUntil(',');
+        // String homeApSSID = homeAPCredentials.readStringUntil(',');
+        // String homeApPSK = homeAPCredentials.readStringUntil(',');
+        String homeApSSID = "Scorpius";
+        String homeApPSK = "LogIn.WiFi.NSMTFS";
         homeAPCredentials.close();
         Serial.println("[SETUP]: Stored credentials found SSID: " + homeApSSID + " PSK: " + homeApPSK);
 
         //WiFi
         Serial.println("[SETUP]: Connecting to WiFi SSID: " + homeApSSID);
+        WIFI::client.setInsecure();
         WiFi.begin(homeApSSID, homeApPSK);
         while (WiFi.status() != WL_CONNECTED) {
             Serial.print(".");
@@ -119,7 +113,7 @@ void setup() {
         Serial.println("\n[WIFI]: Connected to WiFi SSID: " + homeApSSID);
 
         //Updates
-        Serial.println("[SETUP]: Checking for updates");
+        Serial.println("[SETUP]: Checking for OTA updates");
         switch(ESPhttpUpdate.update(WIFI::client, HTTP::HOST, HTTP::HOST_PORT, HTTP::FIRMWARE_URL)) {
             case HTTP_UPDATE_FAILED: {
                 Serial.println("[OTA] Failed");
@@ -145,18 +139,23 @@ void setup() {
 
         Serial.println("[SETUP]: Configuring MQTT");
         MQTT::client.setServer(MQTT::HOST, MQTT::HOST_PORT);
-        MQTT::client.setClientId(MQTT::CLIENT_ID);
-        MQTT::client.setCredentials(MQTT::HOST_USERNAME, MQTT::HOST_PASSWORD);
-        MQTT::client.onConnect(MQTT::onMqttConnect);
-        MQTT::client.onDisconnect(MQTT::onMqttDisconnect);
-        MQTT::client.onPublish(MQTT::onMqttPublish);
-        MQTT::client.onSubscribe(MQTT::onMqttSubscribe);
-        MQTT::client.onMessage(MQTT::onMqttMessage);
-        MQTT::client.connect();
+        MQTT::client.setCallback(MQTT::onMqttMessage);
+
+        Serial.println("[MQTT]: Connecting to broker");
+        while (!MQTT::client.connected()) {
+            if (MQTT::client.connect("helloworld", MQTT::HOST_USERNAME, MQTT::HOST_PASSWORD)) {
+                Serial.println("[MQTT]: Connected BROKER: " + String(MQTT::HOST) + ":" + String(MQTT::HOST_PORT));
+                MQTT::client.subscribe(MQTT::POWER_TOPIC);
+            } else {
+                Serial.print(".");
+                delay(500);
+            }
+        }
 
         while (true) {
             MQTT::client.publish(MQTT::READINGS_TOPIC, 0, true, "[{\"v\":0.123,\"i\":0.345,\"time\":1674890175442},{\"v\":0.456,\"i\":0.456,\"time\":1674890175442},{\"v\":0.123,\"i\":0.345,\"time\":1674890175442},{\"v\":0.789,\"i\":0.567,\"time\":1674890175442}]");
-            delay(10000);
+            
+            MQTT::client.loop();
         }
     } else {
         //CASE: Create a softAP to change the authentication details
@@ -188,7 +187,6 @@ void setup() {
 }
 
 void loop() {
-    // if (WiFi.status() == WL_CONNECTED) {
-    //     webSocketClient.loop();
-    // }
+    Serial.println("[ERR]: Loop reached");
+    delay(2000);
 }
